833812b9809f6e29498b66c309484fbb641f2c34
elst0547 <elijah.staple@colorado.edu>
2015-04-26 13:42:52 -0600

    added files

---
+++ convert.cpp
@@ -0,0 +1,47 @@
+#include "convert.h"
+#include <vector>
+#include <iostream>
+#include <climits>
+#include <queue>
+
+using namespace std;
+
+Graph::Graph()
+{
+
+}
+
+Graph::~Graph()
+{
+
+}
+
+void Graph::addEdge(string v1, string v2, int weight)
+{
+
+}
+
+void Graph::addVertex(string n)
+{
+
+}
+
+void Graph::displayEdges()
+{
+
+}
+
+void Graph::shortestPath(string startingCity, string destination)
+{
+
+}
+
+void Graph::findDistricts()
+{
+
+}
+
+void Graph::shortestDistance(string startingCity, string destination)
+{
+
+}
---
+++ convert.h
@@ -0,0 +1,23 @@
+#ifndef CONVERT_H
+#define CONVERT_H
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream> 
+
+class Converter{
+	private:
+	
+	public:
+		Graph();
+		~Graph();
+		City* NewVCity();
+		void addEdge(std::string v1, std::string v2, int weight);
+		void addVertex(std::string n);
+		void displayEdges();
+		void shortestPath(std::string startingCity, std::string destination);
+		void findDistricts();
+		void shortestDistance(std::string startingCity, std::string destination);
+};
+#endif // CONVERT_H
